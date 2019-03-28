45a2be4dad69f8a8819f62b946d6dcdb0a23c8f6
Vaxure <47508058+Vaxure1337@users.noreply.github.com>
2019-03-27 22:43:31 -0230

    Update

--- Installer.cpp
+++ Installer.cpp
@@ -50,13 +50,33 @@ void choice()
         std::cout << "Wanna start the application? [y/n]" << std::endl;
         LazyStart();
     }
+    if (ch == "2"){
+        printf("Installing Nmap...\n");
+        printf("Updating...\n");
+        system("sudo apt get update");
+        printf("Upgrading...");
+        system("sudo apt get upgrade");
+        printf("apt install...\n");
+        system("sudo apt install nmap");
+        system("clear");
+    }
+    if (ch == "3"){
+        printf("Installing Sqlmap...\n");
+        printf("Updating...\n");
+        system("sudo apt get update");
+        printf("Upgrading...");
+        system("sudo apt get upgrade");
+        printf("apt install...\n");
+        system("sudo apt install sqlmap");
+        system("clear");
+    }
 }
 
 int main()
 {
   std::cout << "-Made by Vaxure" << std::endl;
-  std::cout << "-Version [0.0.1]" << std::endl;
-  std::cout << "1) Install LazySource" << std::endl;
+  std::cout << "-Version [0.0-b2]" << std::endl;
+  std::cout << "1) Install LazySource\n2) Install Nmap\n3) Install Sqlmap" << std::endl;
   choice();
   return 0;
 }
